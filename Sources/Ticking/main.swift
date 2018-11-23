82514ed60cc514a04c34cf449f06fae8e47a93c3
Michael Skiba <mike.skiba@atelierclockwork.net>
2018-11-23 23:09:58 +0100

    relinting

--- Sources/Ticking/main.swift
+++ Sources/Ticking/main.swift
@@ -25,16 +25,18 @@ struct Environment {
     }
 }
 
-func tick(env: Environment, proj: Projectile) -> Projectile {
-    let position = proj.position + proj.velocity
-    let velocity = proj.velocity + env.gravity + env.wind
+func tick(environment: Environment, projectile: Projectile) -> Projectile {
+    let position = projectile.position + projectile.velocity
+    let velocity = projectile.velocity + environment.gravity + environment.wind
     return Projectile(position: position, velocity: velocity)
 }
 
-var p = Projectile(position: .point(x: 0, y: 10, z: 0), velocity: Tuple.vector(x: 1, y: 1, z: 0).normalized)
-let environment = Environment(gravity: .vector(x: 0, y: -0.1, z: 0), wind: .vector(x: -0.1, y: 0, z: 0))
+var projectile = Projectile(position: .point(x: 0, y: 10, z: 0),
+                            velocity: Tuple.vector(x: 1, y: 1, z: 0).normalized)
+let environment = Environment(gravity: .vector(x: 0, y: -0.1, z: 0),
+                              wind: .vector(x: -0.1, y: 0, z: 0))
 
-while p.position.y > 0 {
-    print("\(p.position)")
-    p = tick(env: environment, proj: p)
+while projectile.position.y > 0 {
+    print("\(projectile.position)")
+    projectile = tick(environment: environment, projectile: projectile)
 }
--- Tests/RaytracerChallengeTests/XCTestManifests.swift
+++ Tests/RaytracerChallengeTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(RaytracerChallengeTests.allTests),
+        testCase(RaytracerChallengeTests.allTests)
     ]
 }
 #endif
