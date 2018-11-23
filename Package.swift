3da3ae79f9661b5e734276529b4b19092f687862
Michael Skiba <mike.skiba@atelierclockwork.net>
2018-11-23 23:09:58 +0100

    relinting

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:4.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -11,15 +9,10 @@ let package = Package(
             targets: ["RaytracerChallenge"]),
         .executable(
             name: "Ticking",
-            targets: ["Ticking"]),
+            targets: ["Ticking"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RaytracerChallenge",
             dependencies: []),
@@ -28,6 +21,6 @@ let package = Package(
             dependencies: ["RaytracerChallenge"]),
         .testTarget(
             name: "RaytracerChallengeTests",
-            dependencies: ["RaytracerChallenge"]),
+            dependencies: ["RaytracerChallenge"])
     ]
 )
