1d916e784b60262f6b8f7db9e5336a9f5925e6bd
Michael Skiba <mike.skiba@atelierclockwork.net>
2018-11-25 15:00:56 +0100

    Finished chapter 3

--- Sources/RaytracerChallenge/Matrix.swift
+++ Sources/RaytracerChallenge/Matrix.swift
@@ -17,20 +17,20 @@ public struct Matrix: Equatable {
         matrix = rawMatrix
     }
 
-    public subscript(row row: Int, col col: Int) -> Float {
+    public subscript(row row: Int, column column: Int) -> Float {
         get {
-            return matrix[row][col]
+            return matrix[row][column]
         }
         set {
-            matrix[row][col] = newValue
+            matrix[row][column] = newValue
         }
     }
 
-    var width: Int {
+    public var width: Int {
         return matrix.first?.count ?? 0
     }
 
-    var height: Int {
+    public var height: Int {
         return matrix.count
     }
 
@@ -44,8 +44,8 @@ public struct Matrix: Equatable {
             .reduce(into: Matrix(rows: lhs.width, columns: lhs.height)) { result, position in
             let (y, x) = position
             let row = lhs.matrix[y][size]
-            let col = rhs.matrix.map({ $0[x] })
-            result[row: y, col: x] = zip(row, col).reduce(into: 0) { result, args in
+            let column = rhs.matrix.map({ $0[x] })
+            result[row: y, column: x] = zip(row, column).reduce(into: 0) { result, args in
                 result += args.0 * args.1
             }
         }
@@ -66,24 +66,75 @@ public struct Matrix: Equatable {
         guard lhs.matrix.count == rhs.matrix.count else { return false }
         return zip(lhs.matrix, rhs.matrix).allSatisfy { (lhsRow, rhsRow) in
             guard lhsRow.count == rhsRow.count else { return false }
-            return zip(lhsRow, rhsRow).allSatisfy { (lhsNumber, rhsNumber) in
+            return zip(lhsRow, rhsRow).allSatisfy { set in
+                let (lhsNumber, rhsNumber) = set
                 return lhsNumber.isEqualTo(rhsNumber, epsilon: .epsilon)
             }
         }
     }
 
-    var transposed: Matrix {
+    public var transposed: Matrix {
         return matrix.enumerated().reduce(into: Matrix(rows: width, columns: height, repeating: 0)) { result, row in
-            for (col, value) in row.element.enumerated() {
-                result[row: col, col: row.offset] = value
+            for (column, value) in row.element.enumerated() {
+                result[row: column, column: row.offset] = value
             }
         }
+    }
+
+    public var determinant: Float {
+        assert(width == height, "Determinants need a balanced width / height")
+        assert(width != 1, "Can't calculate determinant on a 1x1 matrix")
+        guard width != 2 else {
+            return self[row: 0, column: 0] * self[row: 1, column: 1] -
+                self[row: 0, column: 1] * self[row: 1, column: 0]
+        }
+        return matrix[0].enumerated().reduce(into: 0) { result, step in
+            result += step.element * cofactor(row: 0, column: step.offset)
+        }
+    }
+
+    public func submatrix(row rowToRemove: Int, column columnToRemove: Int) -> Matrix {
+        assert(rowToRemove < height && columnToRemove < width, "Removed values must be inside the matrix")
+        let submatrix = matrix.enumerated().compactMap { item -> [Float]? in
+            guard item.offset != rowToRemove else { return nil }
+            var row = item.element
+            row.remove(at: columnToRemove)
+            return row
+        }
+        return Matrix(submatrix)
+    }
+
+    public func minor(row: Int, column: Int) -> Float {
+        return submatrix(row: row, column: column).determinant
+    }
+
+    public func cofactor(row: Int, column: Int) -> Float {
+        if (row + column) % 2 == 0 {
+            return minor(row: row, column: column)
+        } else {
+            return -minor(row: row, column: column)
+        }
+    }
+
+    private var inverible: Bool {
+        return self.determinant != 0
+    }
+
+    public var inverse: Matrix? {
+        assert(width == height, "Width must equal height to invert")
+        guard self.inverible else { return nil }
+        let cachedDeterminant = determinant
+        var newMatrix = Matrix(rows: width, columns: height)
+        for (row, column) in allCombinations(0..<width, 0..<height) {
+            newMatrix[row: column, column: row] = cofactor(row: row, column: column) / cachedDeterminant
+        }
+        return newMatrix
     }
 }
 
 private func allCombinations<Collection1, Collection2>(_ lhs: Collection1, _ rhs: Collection2)
     -> [(Collection1.Element, Collection2.Element)] where Collection1: Collection, Collection2: Collection {
-        return lhs.flatMap { (leftItem) -> [(Collection1.Element, Collection2.Element)] in
+        return lhs.flatMap { leftItem in
             return rhs.map { (leftItem, $0) }
         }
 }
